71959bc804fe30e8dda45724f5371f3f33fbc534
J. Graham <jgraham@Js-MacBook-Air.local>
2023-02-13 00:41:44 -0700

    Installed React Router, setup routes where needed, built out ProductScreen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,20 +5,32 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
     return (
         <header>
         {/*variant changes the font color to be opposite of dark making font light */}
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-            <Container>
-                <Navbar.Brand href="/">E-Commerce</Navbar.Brand>
+            <Container>   
+                <LinkContainer to="/">
+                    <Navbar.Brand>E-Commerce</Navbar.Brand>               
+                </LinkContainer>  
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     {/* ms-auto pushes links to far right of container */}
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-                        <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+
+                            <LinkContainer to="/cart">
+                                <Nav.Link >
+                                    <i className='fas fa-shopping-cart'></i> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>
+                                    <i className='fas fa-user'></i> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
