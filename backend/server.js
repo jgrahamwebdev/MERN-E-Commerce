0a94c68ea7c4d0192d52e547a56bb105946144f2
J. Graham <jgraham@Js-MacBook-Air.local>
2023-02-18 02:38:21 -0700

    Setup Postman, created custom error handling Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js';
 
@@ -17,6 +18,10 @@ app.get('/', (req, res) => {
 
 //Mount productRoutes
 app.use('/api/products', productRoutes)
+
+//Custom Error Middlware calls:
+app.use(notFound)
+app.use(errorHandler)
 
 //Environment Variables Implementation:
 const PORT = process.env.PORT || 5000
