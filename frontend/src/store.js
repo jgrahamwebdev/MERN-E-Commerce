01def388fd339189f763056d93522a3685f4de6a
J. Graham <jgraham@Js-MacBook-Air.local>
2023-02-20 02:55:39 -0700

    Built entire cart section, add/remove items, quantity/price updating, etc.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,14 +4,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers';
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
 const store = configureStore({
     reducer: {
         productList: productListReducer,
         productDetails: productDetailsReducer,
-
+        cart: cartReducer,
     },
-    preloadedState: {},
+    preloadedState: {
+        cart: { cartItems: cartItemsFromStorage }
+    },
     middleware: [thunk],
 })
 
